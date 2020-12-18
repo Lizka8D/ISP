72576a950df705a302f0b45377f5b73ff9591698
Быхова Елизавета <65420543+Lizka8D@users.noreply.github.com>
2020-12-19 00:03:26 +0300

    Update Program.cs

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -11,8 +11,18 @@ namespace Lab3
         static void Main()
         {
             Service1 myService = new Service1();
-            myService.OnDebug();
-            Thread.Sleep(Timeout.Infinite);
+            
+            try
+            {
+                ServiceBase.Run(myService);
+            }
+            catch (Exception ex)
+            {
+                using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Exceptions.txt"), true))
+                {
+                    sw.WriteLine($"{DateTime.Now:dd/MM/yyyy HH:mm:ss} Exception: {ex.Message}");
+                }
+            }
         }
     }
 }
