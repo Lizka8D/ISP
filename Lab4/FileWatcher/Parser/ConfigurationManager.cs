fdee799f7e906a8b01514fc3c016d3c89b770d1a
Быхова Елизавета <65420543+Lizka8D@users.noreply.github.com>
2020-12-22 22:47:19 +0300

    Create ConfigurationManager.cs

---
+++ Lab4/FileWatcher/Parser/ConfigurationManager.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Lab3
+{
+    class ConfigurationManager : IParser
+    {
+        private readonly IParser parser;
+        public ConfigurationManager(string path, Type mainType)
+        {
+            if (path.EndsWith(".xml"))
+            {
+                parser = new ParserXML(path, mainType);
+            }
+            else if (path.EndsWith(".json"))
+            {
+                parser = new ParserJson(path, mainType);
+            }
+            else
+            {
+                throw new ArgumentNullException($"invalid extension");
+            }
+        }
+
+        public T GetOptions<T>() => parser.GetOptions<T>();
+    }
+}
