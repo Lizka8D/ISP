fb5e7f747b75e03597bacabdddab9544f16cd8fd
Быхова Елизавета <65420543+Lizka8D@users.noreply.github.com>
2021-01-02 20:48:25 +0300

    Update Service1.cs

--- Lab5/DataManager/Service1.cs
+++ Lab5/DataManager/Service1.cs
@@ -14,37 +14,34 @@ namespace Lab4
     public partial class Service1 : ServiceBase
     {
         readonly DataIO appInsights;
-
         readonly DataOptions dataOptions;
 
         public Service1(DataOptions dataOptions, DataIO appInsights)
         {
             InitializeComponent();
-
             this.dataOptions = dataOptions;
-
             this.appInsights = appInsights;
         }
 
         protected override void OnStart(string[] args)
         {
             DataIO reader = new DataIO(dataOptions.ConnectionString);
-
             FileTransfer fileTransfer = new FileTransfer(dataOptions.TargetFolder, dataOptions.SourcePath);
 
             string customersFileName = "person";
 
-            reader.GetCustomers(dataOptions.TargetFolder, appInsights, customersFileName);
+            await reader.GetCustomers(dataOptions.TargetFolder, appInsights, customersFileName);
 
-            fileTransfer.SendFileToFtp($"{customersFileName}.xml");
-            fileTransfer.SendFileToFtp($"{customersFileName}.xsd");
+            await fileTransfer.SendFileToFtp($"{customersFileName}.xml");
+            await fileTransfer.SendFileToFtp($"{customersFileName}.xsd");
 
-            appInsights.InsertInsight("Files were sent to FTP successfully");
+            await appInsights.InsertInsight("Files were sent to FTP successfully");
         }
 
         protected override void OnStop()
         {
-            appInsights.InsertInsight("Service was successfully stopped");
+            await appInsights.InsertInsightAsync("Stop");
+            await appInsights.WriteInsightsToXmlAsync(dataOptions.OutputFolder);
         }
     }
 }
