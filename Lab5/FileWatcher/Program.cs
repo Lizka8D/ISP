49a7c4c754c883e3844088804639ddc3f8397a2a
Быхова Елизавета <65420543+Lizka8D@users.noreply.github.com>
2021-01-02 16:28:21 +0300

    Create Program.cs

---
+++ Lab5/FileWatcher/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.ServiceProcess;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        static void Main()
+        {
+            Service1 myService = new Service1();
+            
+            try
+            {
+                ServiceBase.Run(myService);
+            }
+            catch (Exception ex)
+            {
+                using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Exceptions.txt"), true))
+                {
+                    sw.WriteLine($"{DateTime.Now:dd/MM/yyyy HH:mm:ss} Exception: {ex.Message}");
+                }
+            }
+        }
+    }
+}
