cc9a14c3f99751d1a67eac22b6e19b45a5176453
Быхова Елизавета <65420543+Lizka8D@users.noreply.github.com>
2021-01-02 16:37:05 +0300

    Create Options.cs

---
+++ Lab5/FileWatcher/Options/Options.cs
@@ -0,0 +1,20 @@
+namespace Lab3
+{
+    class Options
+    {
+        public string SourcePath { get; set; }
+        public string TargetPath { get; set; }
+        public string LogFilePath { get; set; }
+        public bool NeedToEncrypt { get; set; }
+        public ArchiveOptions ArchiveOptions { get; set; }
+        public Options() { }
+        public Options(string sourcePath, string targetPath, string logFilePath, ArchiveOptions archiveOptions, bool needToEncrypt)
+        {
+            SourcePath = sourcePath;
+            TargetPath = targetPath;
+            LogFilePath = logFilePath;
+            ArchiveOptions = archiveOptions;
+            NeedToEncrypt = needToEncrypt;
+        }
+    }
+}
