d1bdbafe5b2f7ef8c7ce5941d1af17e61039cc65
Быхова Елизавета <65420543+Lizka8D@users.noreply.github.com>
2021-01-02 16:36:44 +0300

    Create ArchiveOptions.cs

---
+++ Lab5/FileWatcher/Options/ArchiveOptions.cs
@@ -0,0 +1,9 @@
+namespace Lab3
+{
+    class ArchiveOptions
+    {
+        public ArchiveOptions() { }
+        public bool NeedToMove { get; set; }
+        public bool NeedToCompress { get; set; }
+    }
+}
