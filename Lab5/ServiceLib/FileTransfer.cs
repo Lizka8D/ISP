6a2f72fdac9caf1093fc62a65414dc811a1bf2ad
Быхова Елизавета <65420543+Lizka8D@users.noreply.github.com>
2021-01-02 20:33:36 +0300

    Update and rename Lab5/DataManager/ServiceLib/FileTransfer.cs to Lab5/ServiceLib/FileTransfer.cs

---
+++ Lab5/ServiceLib/FileTransfer.cs
@@ -0,0 +1,29 @@
+using System.IO;
+
+namespace ServiceLib
+{ 
+    public class FileTransfer
+    {
+        readonly string ftpFolder;
+        readonly string outputFolder;
+
+        public FileTransfer(string outputFolder, string ftpFolder)
+        {
+            this.ftpFolder = ftpFolder;
+            this.outputFolder = outputFolder;
+        }
+
+        public Task void SendFileToFtp(string fileName)
+        {
+            return Task.Run(() =>
+            {
+                if (File.Exists(Path.Combine(ftpFolder, fileName)))
+                {
+                    File.Delete(Path.Combine(ftpFolder, fileName));
+                }
+
+                File.Copy(Path.Combine(outputFolder, fileName), Path.Combine(ftpFolder, fileName));
+             });     
+        }
+    }
+}
