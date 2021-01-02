2360f7bb9d4adfb83bd71de18ba4050c05a39da3
Быхова Елизавета <65420543+Lizka8D@users.noreply.github.com>
2021-01-02 20:35:08 +0300

    Update and rename Lab5/DataManager/ServiceLib/XmlGenerator.cs to Lab5/ServiceLib/XmlGenerator.cs

---
+++ Lab5/ServiceLib/XmlGenerator.cs
@@ -0,0 +1,24 @@
+using System.Data;
+using System.IO;
+
+namespace ServiceLib
+{
+    public class XmlGenerator
+    {
+        readonly string outputFolder;
+
+        public XmlGenerator(string outputFolder)
+        {
+            this.outputFolder = outputFolder;
+        }
+
+        public Task WriteToXmlAsync(DataSet dataSet, string fileName)
+        {
+            return Task.Run(() =>
+            {
+                dataSet.WriteXml(Path.Combine(outputFolder, $"{fileName}.xml"));
+                dataSet.WriteXmlSchema(Path.Combine(outputFolder, $"{fileName}.xsd"));
+             });                
+        }
+    }
+}
